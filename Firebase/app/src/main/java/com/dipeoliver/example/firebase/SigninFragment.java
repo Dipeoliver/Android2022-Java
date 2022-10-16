c4ceacf78a0e63626e21bbabe9acef0b0d110542
Diego Oliveira <dipeoliver@outlook.com>
2022-10-15 23:14:03 -0300

    add register method

---
+++ Firebase/app/src/main/java/com/dipeoliver/example/firebase/SigninFragment.java
@@ -0,0 +1,36 @@
+package com.dipeoliver.example.firebase;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SigninFragment extends Fragment {
+
+    private SigninViewModel mViewModel;
+
+    public static SigninFragment newInstance() {
+        return new SigninFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_signin, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SigninViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Firebase/app/src/main/java/com/dipeoliver/example/firebase/SigninViewModel.java
@@ -0,0 +1,7 @@
+package com.dipeoliver.example.firebase;
+
+import androidx.lifecycle.ViewModel;
+
+public class SigninViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
